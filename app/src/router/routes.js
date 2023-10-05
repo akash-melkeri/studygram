70ea762eae6d0c9013f10adeaeb8f81293435548
akash-melkeri <akashmelkeri23@gmail.com>
2023-10-05 11:56:47 +0530

    router

--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -4,12 +4,16 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      {
+        path: '/',
+        component: () => import('pages/Home.vue'),
+        meta:{
+          is_public:false,
+        }
+      },
     ]
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
