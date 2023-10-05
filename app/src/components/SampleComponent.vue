018140ecec6a45c91a2d4c1f3b9d9aab7d03f575
akash-melkeri <akashmelkeri23@gmail.com>
2023-10-05 11:15:37 +0530

    sample component

---
+++ app/src/components/SampleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'EssentialLink',
+  props: {
+    test:{
+      default:'test'
+    }
+  },
+  emits:[],
+  setup(){
+    return{
+
+    }
+  },
+  mounted(){
+
+  },
+  created(){
+
+  }
+
+})
+</script>
