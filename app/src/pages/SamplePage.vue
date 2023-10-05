31ffe4fb39f1e5abbbeb381454719707da927885
akash-melkeri <akashmelkeri23@gmail.com>
2023-10-05 11:27:48 +0530

    sample page

---
+++ app/src/pages/SamplePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Sample Page
+  </div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'SamplePage',
+  components:{},
+  setup(){
+    return{
+
+    }
+  },
+  computed:{
+
+  },
+  mounted(){
+
+  },
+  created(){
+
+  }
+
+})
+</script>
