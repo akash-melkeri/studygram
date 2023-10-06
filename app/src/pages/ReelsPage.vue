e1d2be83811ae6b7e231943c1259d3793233ce32
akash-melkeri <akashmelkeri23@gmail.com>
2023-10-06 16:05:15 +0530

    text

--- app/src/pages/ReelsPage.vue
+++ app/src/pages/ReelsPage.vue
@@ -1,7 +1,14 @@
 <template>
-  <q-page class="flex flex-center">
-    Reels Page
-  </q-page>
+  <div>
+    <q-header class="tw-bg-white tw-text-black tw-border-b-[0.0001em] tw-border-b-gray-200">
+      <q-toolbar class="bg-transparent tw-flex ">
+        <svg xmlns="http://www.w3.org/2000/svg" class="tw-w-6 tw-h-6 tw-mr-3" viewBox="0 0 24 24"><path fill="currentColor" d="m7.825 13l4.9 4.9q.3.3.288.7t-.313.7q-.3.275-.7.288t-.7-.288l-6.6-6.6q-.15-.15-.213-.325T4.425 12q0-.2.063-.375T4.7 11.3l6.6-6.6q.275-.275.688-.275t.712.275q.3.3.3.713t-.3.712L7.825 11H19q.425 0 .713.288T20 12q0 .425-.288.713T19 13H7.825Z"/></svg>
+      </q-toolbar>
+    </q-header>
+    <q-page class="flex flex-center">
+      Reels
+    </q-page>
+  </div>
 </template>
 <script>
 import { defineComponent, ref } from 'vue'
